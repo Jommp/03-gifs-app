2754a0b005c0e6893fa5db48b3620a153a047793
Jommp <jommp.dev@gmail.com>
2025-09-08 22:07:15 -0500

    feat(SearchBar): implement methods to handle query search

--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,14 +1,36 @@
+import { useState, type KeyboardEvent } from "react";
+
 interface Props {
   placeholder: string;
   button?: string;
+  onQuery: (query: string) => void;
 };
 
-export const SearchBar = ({ placeholder, button = 'Buscar' }: Props) => {
+export const SearchBar = ({ placeholder, button = 'Buscar', onQuery }: Props) => {
+  const [query, setQuery] = useState('');
+  
+  const handleSearch = () => {
+    onQuery(query);
+  };
+
+  const handleKeyDown = (key: KeyboardEvent<HTMLInputElement>) => {
+    if (key.key !== 'Enter') return;
+
+    handleSearch();
+  };
+
   return (
     <section className="search-container">
-      <input type="text" placeholder={ placeholder } />
+      <input
+        type="text"
+        placeholder={ placeholder }
+        onChange={(ev) => setQuery(ev.target.value)}
+        onKeyDown={handleKeyDown}
+      />
 
-      <button>{ button }</button>
+      <button onClick={handleSearch}>
+        { button }
+      </button>
     </section>
   );
 };
