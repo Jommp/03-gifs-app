0eba8e7b64c50e72ffaa1caade6e82b2fa8551ac
Jommp <jommp.dev@gmail.com>
2025-09-25 12:04:11 -0500

    feat(): fix type issues to run build

--- src/gifs/interfaces/giphy.response.ts
+++ src/gifs/interfaces/giphy.response.ts
@@ -5,7 +5,6 @@ export interface GiphyResponse {
 }
 
 export interface GiphyGif {
-  type: Type;
   id: string;
   url: string;
   slug: string;
@@ -15,14 +14,12 @@ export interface GiphyGif {
   username: string;
   source: string;
   title: string;
-  rating: Rating;
   content_url: string;
   source_tld: string;
   source_post_url: string;
   source_caption?: string;
   is_sticker: number;
   import_datetime: Date;
-  trending_datetime: Date | TrendingDatetimeEnum;
   images: Images;
   user?: User;
   analytics_response_payload: string;
@@ -62,18 +59,6 @@ export interface FixedHeight {
   webp: string;
   frames?: string;
   hash?: string;
-}
-
-export enum Rating {
-  G = "g",
-}
-
-export enum TrendingDatetimeEnum {
-  The00000000000000 = "0000-00-00 00:00:00",
-}
-
-export enum Type {
-  GIF = "gif",
 }
 
 export interface User {
